2d65be62b8eaeec5e037a869ed2e1352fb15eb84
Tanner Bennett <tannerbennett@me.com>
2017-03-29 17:43:57 -0500

    TBInfoView for headers

---
+++ TBTweakViewController/TBTweakViewController/Classes/TBInfoView.h
@@ -0,0 +1,18 @@
+//
+//  TBInfoView.h
+//  TBTweakViewController
+//
+//  Created by Tanner on 3/29/17.
+//  Copyright © 2017 Tanner Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TBInfoView : UIView
+
++ (instancetype)text:(NSString *)text;
+
+@property (nonatomic, readonly) UIView *hairline;
+
+@end
---
+++ TBTweakViewController/TBTweakViewController/Classes/TBInfoView.m
@@ -0,0 +1,73 @@
+//
+//  TBInfoView.m
+//  TBTweakViewController
+//
+//  Created by Tanner on 3/29/17.
+//  Copyright © 2017 Tanner Bennett. All rights reserved.
+//
+
+#import "TBInfoView.h"
+#import <Masonry.h>
+
+
+@interface TBInfoView ()
+@property (nonatomic, readonly) UILabel *label;
+@end
+
+@implementation TBInfoView
+
++ (instancetype)text:(NSString *)text {
+    TBInfoView *view = [self new];
+    view.label.text  = text;
+    return view;
+}
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self configure];
+    }
+
+    return self;
+}
+
+- (id)init {
+    return [self initWithFrame:CGRectZero];
+}
+
+- (void)configure {
+    _label = [[UILabel alloc] initWithFrame:CGRectZero];
+    _hairline = [[UIView alloc] initWithFrame:CGRectZero];
+
+    self.label.numberOfLines = 0;
+    self.label.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+//    self.label.font = [UIFont preferredFontForTextStyle:UIFontTextStyleFootnote];
+
+    self.backgroundColor = [UIColor colorWithWhite:0.937 alpha:1.000];
+//    self.backgroundColor = [UIColor colorWithRed:0.937 green:0.937 blue:0.957 alpha:1.000];
+    self.label.textColor = [UIColor blackColor];
+//    self.label.textColor = [UIColor colorWithRed:0.427 green:0.427 blue:0.447 alpha:1.000];
+    self.hairline.backgroundColor = [UIColor colorWithWhite:0.698 alpha:1.000];
+
+    [self addSubview:self.label];
+    [self addSubview:self.hairline];
+}
+
++ (BOOL)requiresConstraintBasedLayout {
+    return YES;
+}
+
+- (void)updateConstraints {
+    [self.label mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self).insets(UIEdgeInsetsMake(10, 15, 10, 15));
+//        make.edges.equalTo(self).insets(UIEdgeInsetsMake(15, 15, 15, 15));
+    }];
+    [self.hairline mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.width.bottom.equalTo(self);
+        make.height.equalTo(@0.5);
+    }];
+
+    [super updateConstraints];
+}
+
+@end
